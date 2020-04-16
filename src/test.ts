23bc15036568dc3f6dbe155c365db71033ae7b96
Salvador <salvasanzandres@gmail.com>
2020-04-16 19:47:49 +0200

    update manifest

--- src/test.ts
+++ src/test.ts
@@ -22,6 +22,6 @@ getTestBed().initTestEnvironment(
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
 
-// const context = require.context('./app/core', true, /^((?!guard).)*\.spec.ts$/);
+// const context = require.context('./app/core/services/', true, /^((?!guard).)*\.spec.ts$/);
 // And load the modules.
 context.keys().map(context);
